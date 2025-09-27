2ec5377cfcca51527f608165561a7f65ed26a904
ZaineM987 <zainem@hotmail.com>
2025-09-27 20:22:56 +1200

    Initial commit: Add project structure and Main.java

---
+++ src/com/task9demo/Main.java
@@ -0,0 +1,10 @@
+package com.task9demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    // TODO Auto-generated method stub
+	    System.out.println("This is the initial version of the project.");
+	}
+
+}
